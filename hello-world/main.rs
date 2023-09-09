498568830504c03f2f40824d3734193b30970dba
Santosh Mahto <santosh.mahto@pw.live>
2023-09-09 16:27:01 +0530

    learn basic

--- hello-world/main.rs
+++
@@ -1,8 +0,0 @@
-fn main(){
-    println!("Hello, World!");
-    author();
-}
-
-fn author(){
-    println!("Santosh Mahto");
-}
---
+++ main.rs
@@ -0,0 +1,8 @@
+fn main(){
+    println!("Hello, World! 🙏🏼");
+    author();
+}
+
+fn author(){
+    println!("Namaste Rust");
+}
